8383ab87477739cd4b15ac7a6117881ad94438b9
Jan-Oliver Pantel <janpantel@users.noreply.github.com>
2021-08-11 12:12:58 +0200

    Move code from analytics repository

---
+++ Shim.lua
@@ -0,0 +1,27 @@
+local WagoAnalyticsShim = LibStub:NewLibrary("WagoAnalytics", 1)
+
+function WagoAnalyticsShim:Register(wagoID)
+	local WagoAnalytics = WagoAnalytics
+	if WagoAnalytics then
+		return WagoAnalytics:Register(wagoID)
+	else
+		return setmetatable({}, {
+			__index = {
+				Counter = function() end,
+				Gauge = function() end,
+				Error = function() end,
+				Breadcrumb = function() end
+			}
+		})
+	end
+end
+
+function WagoAnalyticsShim:RegisterAddon(addonName)
+	local wagoID = GetAddOnMetadata(addonName, "X-Wago-ID")
+	if not wagoID then
+		return false
+	end
+	return self:Register(wagoID)
+end
+
+WagoAnalyticsShim.RegisterAddOn = WagoAnalyticsShim.RegisterAddon
